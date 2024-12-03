6353bdb3601bf5838875a284a4f58395ec959ef4
dhruvalgondaliya <f342dhruval@gmail.com>
2024-12-03 15:54:19 +0530

    style:about component -complete all section

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { Routes ,Route} from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home.jsx'
+import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
+import School from './Pages/School.jsx';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about-us' element={<About />} />
+        <Route path='/about-us' element={<School />} />
+
       </Routes>
     </>
   );
