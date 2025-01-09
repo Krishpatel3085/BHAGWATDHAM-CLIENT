40300f6b1f0f2dce123e204de0fdd9f4fe7c1e2c
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2025-01-09 19:04:13 +0530

    Feat : Marksheet Update & Delet Functionality

--- server/src/Routes/marksheet.js
+++ server/src/Routes/marksheet.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { CreateMarksheet } = require('../Controller/marksheet');
+const { CreateMarksheet, GetAllMarksheets, UpdateMarksheet, DeleteMarksheet } = require('../Controller/marksheet');
 
 const marksheet_router = express.Router()
 
 marksheet_router.post('/CreateMarksheet', CreateMarksheet);
-// marksheet_router.get('/GetLecture', getLectures);
-// marksheet_router.put('/UpdateLecture/:id', updateLecture);
-// marksheet_router.delete('/DeleteLecture', deleteLecture);
+marksheet_router.get('/GetMarksheets', GetAllMarksheets);
+marksheet_router.put('/UpdateMarksheet/:id', UpdateMarksheet);
+marksheet_router.delete('/DeleteMarksheet', DeleteMarksheet);
 
-module.exports = { marksheet_router}
+module.exports = { marksheet_router }
 
 
