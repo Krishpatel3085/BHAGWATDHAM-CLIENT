cf56eb74011e05a8ad9faa362a302e9f43f241ea
Krish Kaila <145528945+Krishpatel3085@users.noreply.github.com>
2025-01-15 17:49:11 +0530

    Feat : Add Markshet getById Logic

--- server/src/Controller/marksheet.js
+++ server/src/Controller/marksheet.js
@@ -5,13 +5,12 @@ const StudentSchema = require('../Model/student')
 const CreateMarksheet = async (req, res) => {
     try {
         const { studentName, rollNo, Class, examType, totalMarks, percentage, result, subjects } = req.body;
-        console.log("Marksheet", req.body)
-        const Student = req.user["id"]
-
         const studentID = await StudentSchema.findOne({ studentId: rollNo, name: studentName });
         if (!studentID) {
             return res.status(404).json({ error: 'Student not found' });
         }
+
+        const Student = studentID.Student;
 
         const newMarksheet = await MarksheetSchema.create({
             studentName,
@@ -23,6 +22,7 @@ const CreateMarksheet = async (req, res) => {
             result,
             subjects: subjects,
             Student
+
         });
 
         res.status(201).json({ Message: "Marksheet Created", newMarksheet })
@@ -47,12 +47,11 @@ const GetAllMarksheets = async (req, res) => {
 const GetMarksheetById = async (req, res) => {
     try {
         const { id: _id } = req.params;
-        console.log(_id)
-        const marksheet = await MarksheetSchema.findOne({ Student: _id });
-        if (!marksheet) {
+        const marksheets = await MarksheetSchema.findOne({ Student: _id });
+        if (!marksheets) {
             return res.status(404).json({ error: 'Marksheet not found' });
         }
-        res.status(200).json({ marksheet })
+        res.status(200).json({ marksheets })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
